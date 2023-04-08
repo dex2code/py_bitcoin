d44f2c5dcad5c96a04303dfaf6946cab9ea02190
dex2code <dex2mine@gmail.com>
2023-04-08 19:09:16 +0300

    commit

--- src/curves_test.py
+++ src/curves_test.py
@@ -1,65 +1,70 @@
-from .. import Curve
-from .. import Field
-import pytest
+import unittest
+from py_bitcoin.curves import Point
 
 
-def test_point():
-    assert Curve.Point(x=-1, y=-1, a=5, b=7)
-    with pytest.raises(ValueError):
-        Curve.Point(x=-1, y=-2, a=5, b=7)
+class CurvesTest(unittest.TestCase):
+
+    def test_point(self):
+        self.assertTrue(Point(x=-1, y=-1, a=5, b=7))
+        with self.assertRaises(ValueError):
+            Point(x=-1, y=-2, a=5, b=7)
 
 
-def test_equality():
-    p1 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    p2 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    assert p1 == p2
+    def test_equality(self):
+        p1 = Point(x=-1, y=-1, a=5, b=7)
+        p2 = Point(x=-1, y=-1, a=5, b=7)
+        self.assertTrue(p1 == p2)
 
-    p1 = Curve.Point(x=None, y=None, a=5, b=7)
-    p2 = Curve.Point(x=None, y=None, a=5, b=7)
-    assert p1 == p2
-
-
-def test_not_equality():
-    p1 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    p2 = Curve.Point(x=-1, y=1, a=5, b=7)
-    assert not p1 == p2
+        p1 = Point(x=None, y=None, a=5, b=7)
+        p2 = Point(x=None, y=None, a=5, b=7)
+        self.assertTrue(p1 == p2)
 
 
-def test_addition():
-    p1 = Curve.Point(x=0, y=0, a=5, b=0)
-    p2 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    with pytest.raises(TypeError):
-        p1 + p2
+    def test_not_equality(self):
+        p1 = Point(x=-1, y=-1, a=5, b=7)
+        p2 = Point(x=-1, y=1, a=5, b=7)
+        self.assertFalse(p1 == p2)
 
-    p1 = Curve.Point(x=None, y=None, a=3, b=7)
-    p2 = Curve.Point(x=18, y=77, a=5, b=7)
-    with pytest.raises(TypeError):
-        p1 + p2
 
-    p1 = Curve.Point(x=0, y=0, a=3, b=0)
-    p2 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    with pytest.raises(TypeError):
-        p1 + p2
+    def test_addition(self):
+        p1 = Point(x=0, y=0, a=5, b=0)
+        p2 = Point(x=-1, y=-1, a=5, b=7)
+        with self.assertRaises(TypeError):
+            p1 + p2
 
-    p1 = Curve.Point(x=0, y=0, a=5, b=0)
-    p2 = Curve.Point(x=None, y=None, a=5, b=0)
-    assert p1 + p1 == p2
-    assert not p1 + p1 == p1
+        p1 = Point(x=None, y=None, a=3, b=7)
+        p2 = Point(x=18, y=77, a=5, b=7)
+        with self.assertRaises(TypeError):
+            p1 + p2
 
-    p1 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    p2 = Curve.Point(x=-1, y=1, a=5, b=7)
-    p3 = Curve.Point(x=None, y=None, a=5, b=7)
-    p4 = Curve.Point(x=-1, y=1, a=5, b=7)
-    assert p1 + p2 == p3
-    assert not p1 + p2 == p4
+        p1 = Point(x=0, y=0, a=3, b=0)
+        p2 = Point(x=-1, y=-1, a=5, b=7)
+        with self.assertRaises(TypeError):
+            p1 + p2
 
-    p1 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    p2 = Curve.Point(x=18, y=77, a=5, b=7)
-    assert p1 + p1 == p2
-    assert not p1 + p1 == p1
+        p1 = Point(x=0, y=0, a=5, b=0)
+        p2 = Point(x=None, y=None, a=5, b=0)
+        self.assertTrue((p1 + p1) == p2)
+        self.assertFalse((p1 + p1) == p1)
 
-    p1 = Curve.Point(x=2, y=5, a=5, b=7)
-    p2 = Curve.Point(x=-1, y=-1, a=5, b=7)
-    p3 = Curve.Point(x=3, y=-7, a=5, b=7)
-    assert p1 + p2 == p3
-    assert not p1 + p2 == p2
+        p1 = Point(x=-1, y=-1, a=5, b=7)
+        p2 = Point(x=-1, y=1, a=5, b=7)
+        p3 = Point(x=None, y=None, a=5, b=7)
+        p4 = Point(x=-1, y=1, a=5, b=7)
+        self.assertTrue((p1 + p2) == p3)
+        self.assertFalse((p1 + p2) == p4)
+
+        p1 = Point(x=-1, y=-1, a=5, b=7)
+        p2 = Point(x=18, y=77, a=5, b=7)
+        self.assertTrue((p1 + p1) == p2)
+        self.assertFalse((p1 + p1) == p1)
+
+        p1 = Point(x=2, y=5, a=5, b=7)
+        p2 = Point(x=-1, y=-1, a=5, b=7)
+        p3 = Point(x=3, y=-7, a=5, b=7)
+        self.assertTrue((p1 + p2) == p3)
+        self.assertFalse((p1 + p2) == p1)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/fields_test.py
+++ src/fields_test.py
@@ -2,7 +2,7 @@ import unittest
 from py_bitcoin.fields import FieldElement
 
 
-class fields_test(unittest.TestCase):
+class FieldsTest(unittest.TestCase):
 
     def test_valid_points(self):
         self.assertTrue(FieldElement(num=3, prime=7))
@@ -12,125 +12,124 @@ class fields_test(unittest.TestCase):
     def test_invalid_points(self):
         with self.assertRaises(ValueError):
             FieldElement(num=-3, prime=7)
+        with self.assertRaises(ValueError):
             FieldElement(num=13, prime=7)
+
+
+    def test_equality(self):
+        e1 = FieldElement(11, 13)
+        e2 = FieldElement(11, 13)
+        self.assertTrue(e1 == e2)
+
+
+    def test_not_equality(self):
+        e1 = FieldElement(11, 13)
+        e2 = FieldElement(7, 13)
+        self.assertFalse(e1 == e2)
+
+
+    def test_addition(self):
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(7, 13)
+        e3 = FieldElement(10, 13)
+        e4 = FieldElement(1, 13)
+        self.assertTrue((e1 + e2) == e3)
+        self.assertFalse((e1 + e2) == e4)
+
+        e1 = FieldElement(11, 13)
+        e2 = FieldElement(7, 13)
+        e3 = FieldElement(5, 13)
+        e4 = FieldElement(8, 13)
+        self.assertTrue((e1 + e2) == e3)
+        self.assertFalse((e1 + e2) == e4)
+
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(7, 11)
+        with self.assertRaises(TypeError):
+            e1 + e2
+
+
+    def test_subtraction(self):
+        e1 = FieldElement(7, 13)
+        e2 = FieldElement(3, 13)
+        e3 = FieldElement(4, 13)
+        e4 = FieldElement(1, 13)
+        self.assertTrue((e1 - e2) == e3)
+        self.assertFalse((e1 - e2) == e4)
+
+        e1 = FieldElement(7, 13)
+        e2 = FieldElement(11, 13)
+        e3 = FieldElement(9, 13)
+        e4 = FieldElement(8, 13)
+        self.assertTrue((e1 - e2) == e3)
+        self.assertFalse((e1 - e2) == e4)
+
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(7, 11)
+        with self.assertRaises(TypeError):
+            e1 - e2
+
+
+    def test_multiplication(self):
+        e1 = FieldElement(5, 17)
+        e2 = FieldElement(3, 17)
+        e3 = FieldElement(15, 17)
+        e4 = FieldElement(10, 17)
+        self.assertTrue((e1 * e2) == e3)
+        self.assertFalse((e1 * e2) == e4)
+
+        e1 = FieldElement(7, 13)
+        e2 = FieldElement(11, 13)
+        e3 = FieldElement(12, 13)
+        e4 = FieldElement(8, 13)
+        self.assertTrue((e1 * e2) == e3)
+        self.assertFalse((e1 * e2) == e4)
+
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(7, 11)
+        with self.assertRaises(TypeError):
+            e1 * e2
+
+
+    def test_division(self):
+        e1 = FieldElement(3, 31)
+        e2 = FieldElement(24, 31)
+        e3 = FieldElement(4, 31)
+        e4 = FieldElement(10, 31)
+        self.assertTrue((e1 / e2) == e3)
+        self.assertFalse((e1 / e2) == e4)
+
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(5, 13)
+        e3 = FieldElement(11, 13)
+        e4 = FieldElement(8, 13)
+        self.assertTrue((e1 / e2) == e3)
+        self.assertFalse((e1 / e2) == e4)
+
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(7, 11)
+        with self.assertRaises(TypeError):
+            e1 / e2
+        
+        e1 = FieldElement(3, 13)
+        e2 = FieldElement(0, 13)
+        with self.assertRaises(ZeroDivisionError):
+            e1 / e2
+
+
+    def test_exponentiation(self):
+        e1 = FieldElement(7, 17)
+        e2 = FieldElement(11, 17)
+        e3 = FieldElement(3, 17)
+        self.assertTrue((e1**5) == e2)
+        self.assertFalse((e1**5) == e3)
+
+        e1 = FieldElement(17, 31)
+        e2 = FieldElement(29, 31)
+        e3 = FieldElement(3, 31)
+        self.assertTrue((e1**-3) == e2)
+        self.assertFalse((e1**-3) == e3)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-"""
-def test_equality():
-    e1 = Field.FieldElement(11, 13)
-    e2 = Field.FieldElement(11, 13)
-    assert e1 == e2
-
-
-def test_not_equality():
-    e1 = Field.FieldElement(11, 13)
-    e2 = Field.FieldElement(7, 13)
-    assert not e1 == e2
-
-
-def test_addition():
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(7, 13)
-    e3 = Field.FieldElement(10, 13)
-    e4 = Field.FieldElement(1, 13)
-    assert (e1 + e2) == e3
-    assert not (e1 + e2) == e4
-
-    e1 = Field.FieldElement(11, 13)
-    e2 = Field.FieldElement(7, 13)
-    e3 = Field.FieldElement(5, 13)
-    e4 = Field.FieldElement(8, 13)
-    assert (e1 + e2) == e3
-    assert not (e1 + e2) == e4
-
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(7, 11)
-    with pytest.raises(TypeError):
-        e3 = e1 + e2
-
-
-def test_subtraction():
-    e1 = Field.FieldElement(7, 13)
-    e2 = Field.FieldElement(3, 13)
-    e3 = Field.FieldElement(4, 13)
-    e4 = Field.FieldElement(1, 13)
-    assert (e1 - e2) == e3
-    assert not (e1 - e2) == e4
-
-    e1 = Field.FieldElement(7, 13)
-    e2 = Field.FieldElement(11, 13)
-    e3 = Field.FieldElement(9, 13)
-    e4 = Field.FieldElement(8, 13)
-    assert (e1 - e2) == e3
-    assert not (e1 - e2) == e4
-
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(7, 11)
-    with pytest.raises(TypeError):
-        e3 = e1 - e2
-
-
-def test_multiplication():
-    e1 = Field.FieldElement(5, 17)
-    e2 = Field.FieldElement(3, 17)
-    e3 = Field.FieldElement(15, 17)
-    e4 = Field.FieldElement(10, 17)
-    assert (e1 * e2) == e3
-    assert not (e1 * e2) == e4
-
-    e1 = Field.FieldElement(7, 13)
-    e2 = Field.FieldElement(11, 13)
-    e3 = Field.FieldElement(12, 13)
-    e4 = Field.FieldElement(8, 13)
-    assert (e1 * e2) == e3
-    assert not (e1 * e2) == e4
-
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(7, 11)
-    with pytest.raises(TypeError):
-        e3 = e1 * e2
-
-
-def test_division():
-    e1 = Field.FieldElement(3, 31)
-    e2 = Field.FieldElement(24, 31)
-    e3 = Field.FieldElement(4, 31)
-    e4 = Field.FieldElement(10, 31)
-    assert (e1 / e2) == e3
-    assert not (e1 / e2) == e4
-
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(5, 13)
-    e3 = Field.FieldElement(11, 13)
-    e4 = Field.FieldElement(8, 13)
-    assert (e1 / e2) == e3
-    assert not (e1 / e2) == e4
-
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(7, 11)
-    with pytest.raises(TypeError):
-        e3 = e1 / e2
-    
-    e1 = Field.FieldElement(3, 13)
-    e2 = Field.FieldElement(0, 13)
-    with pytest.raises(ZeroDivisionError):
-        e3 = e1 / e2
-
-
-def test_exponentiation():
-    e1 = Field.FieldElement(7, 17)
-    e2 = Field.FieldElement(11, 17)
-    e3 = Field.FieldElement(3, 17)
-    assert (e1**5) == e2
-    assert not (e1**5) == e3
-
-    e1 = Field.FieldElement(17, 31)
-    e2 = Field.FieldElement(29, 31)
-    e3 = Field.FieldElement(3, 31)
-    assert (e1**-3) == e2
-    assert not (e1**-3) == e3
-"""
