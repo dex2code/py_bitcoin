5da17a97a3b3b13fffa12264caa938fc5261719c
dex2code <dex2mine@gmail.com>
2023-04-22 14:57:31 +0300

    commit

--- lessons/ch05/notepad_01.py
+++ lessons/ch05/notepad_01.py
@@ -3,14 +3,12 @@ import os, sys
 parent = os.path.abspath('.')
 sys.path.insert(1, parent)
 
-from src.py_bitcoin import S256_Gx, S256_Gy, S256_N
-from src.py_bitcoin.u_tools import get_hash256, get_random_secret, get_deterministic_k
-from src.py_bitcoin.fields import S256_FieldElement
-from src.py_bitcoin.curves import S256_Point
-from src.py_bitcoin.skeeper import Wallet, Signature
-import json
-from src.py_bitcoin.skeeper import Secretary
-from src.py_bitcoin.transactions import Tx
+from src.py_bitcoin import *
+from src.py_bitcoin.u_tools import *
+from src.py_bitcoin.fields import *
+from src.py_bitcoin.curves import *
+from src.py_bitcoin.skeeper import *
+from src.py_bitcoin.transactions import *
 
 
 my_tx = Tx(version=1, ins=[1, 2], outs=[3, 4], locktime=0, testnet=False)
